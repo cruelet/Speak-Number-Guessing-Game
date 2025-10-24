f4395f42b95d374fcfe4b1aec66362d8b37654f2
unknown <sawrivs@gmail.com>
2025-10-24 17:59:19 -0400

    refactor to switch case

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ recognition.start();
 function onSpeak(event) {
     const msg = event.results[0][0].transcript;
     writeMessage(msg);
+    checkNumber(msg);
 }
 
 // Listen to and handle the speech event
@@ -34,3 +35,70 @@ function writeMessage(msg) {
    span.textContent = msg;
    msgEl.append(div, span);
 }
+
+// Check msg against the secret number
+function checkNumber(msg) {
+  let num = Number(msg);
+
+  // Update the value of num if it's a single-digit number (refactored)
+  const wordToNumber = {
+    one: 1,
+    won: 1,
+    two: 2,
+    to: 2,
+    too: 2,
+    three: 3,
+    four: 4,
+    for: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    ate: 8,
+    nine: 9,
+    ten: 10,
+  };
+
+  // Check if the spoken content is a valid number
+  if (Number.isNaN(num)) {
+    const div = document.createElement('div');
+    div.textContent = 'That is not a valid number';
+    msgEl.innerHTML = '';
+    msgEl.append(div);
+    return;
+  }
+
+    // Check if it's in range
+    if (num < 1 || num > 100) {
+        const div = document.createElement('div');
+        div.textContent = "Number must be between 1 and 100";
+        msgEl.append(div);
+        return;
+    }
+
+    // Check the number and provide feedback
+if (num === randomNum) {
+    const h2 = document.createElement('h2');
+    h2.textContent = `Congrats! You have guessed the number! It was ${num}`;
+
+    const button = document.createElement('button');
+    button.classList.add('play-again');
+    button.textContent = 'Play again';
+    // Add listener and handler to button
+    button.addEventListener('click', () => window.location.reload());
+
+    msgEl.append(h2, button);
+  } else if (num > randomNum) {
+    const div = document.createElement('div');
+    div.textContent = 'GO LOWER';
+    msgEl.append(div);
+  } else {
+    // if (num < randomNum)
+    const div = document.createElement('div');
+    div.textContent = 'GO HIGHER';
+    msgEl.append(div);
+  }
+}
+
+// At the end of the SpeechRecognition service, start it again
+recognition.addEventListener('end', () => recognition.start());
