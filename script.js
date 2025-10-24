0bbb27261fa7664e2ff017d7bf4574cf5c7ab1cd
unknown <sawrivs@gmail.com>
2025-10-24 15:05:07 -0400

    create speech recognition and implement onspeak event

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const msgEl = document.getElementById('msg');
 function generateRandomNumber() {
     return Math.floor(Math.random() * 100 + 1);
 }
+
+const randomNum = generateRandomNumber();
+//console.log(randomNum);
+
+window.SpeechRecognition = window.SpeedRecognition || window.webkitSpeechRecognition
+
+let recognition = new window.SpeechRecognition();
+
+// Start recognition and game
+recognition.start();
+
+// Capture user speech
+function onSpeak(event) {
+    const msg = event.results[0][0].transcript;
+
+    console.log(msg);
+}
+
+// Listen to and handle the speech event
+recogniton.addEventListener('result', onSpeak);
