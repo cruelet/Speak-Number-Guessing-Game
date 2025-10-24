56622c5b2c0c67fedd8a509318102f68a8cf0d00
unknown <sawrivs@gmail.com>
2025-10-24 15:58:56 -0400

    implement writeMessage(msg) securely

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const msgEl = document.getElementById('msg');
 
 // Generate a random number
 function generateRandomNumber() {
-    return Math.floor(Math.random() * 100 + 1);
+    return Math.floor(Math.random() * 100) + 1;
 }
 
-const randomNum = generateRandomNumber();
-//console.log(randomNum);
+const randomNum = generateRandomNumber()
+console.log(randomNum);
 
-window.SpeechRecognition = window.SpeedRecognition || window.webkitSpeechRecognition
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
 let recognition = new window.SpeechRecognition();
 
@@ -18,9 +19,18 @@ recognition.start();
 // Capture user speech
 function onSpeak(event) {
     const msg = event.results[0][0].transcript;
-
-    console.log(msg);
+    writeMessage(msg);
 }
 
 // Listen to and handle the speech event
-recogniton.addEventListener('result', onSpeak);
+recognition.addEventListener('result', onSpeak);
+
+// See in the DOM what the user has spoken
+function writeMessage(msg) {
+   const div = document.createElement('div');
+   div.textContent = 'You said: ';
+   const span = document.createElement('span');
+   span.classList.add('box');
+   span.textContent = msg;
+   msgEl.append(div, span);
+}
